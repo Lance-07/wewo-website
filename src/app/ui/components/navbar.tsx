de73c75b122ec788b3df3c81bcba1c1d70b4ab58
ArnieM24 <arniemanumbaga@gmail.com>
2025-02-10 00:19:19 +0800

    articles page

--- src/app/ui/components/navbar.tsx
+++ src/app/ui/components/navbar.tsx
@@ -1,56 +1,65 @@
 import Image from "next/image";
 import Link from "next/link";
 import Button from "./button";
-import {poppins} from "@/app/ui/fonts";
+import { poppins } from "@/app/ui/fonts";
 
-const navList: {name: string, link: string}[] = [
+const navList: { name: string; link: string }[] = [
   {
-    name: 'Impacts',
-    link: '#impacts'
+    name: "Impacts",
+    link: "#impacts",
   },
   {
-    name: 'About',
-    link: '#about'
+    name: "About",
+    link: "#about",
   },
   {
-    name: 'How It Works',
-    link: '#how-it-works'
+    name: "How It Works",
+    link: "#how-it-works",
   },
   {
-    name: 'Importance',
-    link: '#importance'
+    name: "Importance",
+    link: "#importance",
   },
   {
-    name: 'FAQs',
-    link: '#faqs'
-  }
-]
+    name: "FAQs",
+    link: "#faqs",
+  },
+  {
+    name: "Articles",
+    link: "#articles",
+  },
+];
 
 export default function Navbar() {
-
   return (
     <nav className="bg-white flex box- justify-center fixed top-0 w-full z-50 h-[60px] shadow-[0_4px_4px_rgba(0_,0_,0_,15%)]">
       <div className="container px-4 mx-auto flex justify-between items-center">
         <div className="flex gap-4 items-center justify-center h-full">
-          <Link href={'/'}>
+          <Link href={"/"}>
             <Image src="/icons/logo.png" width={140} height={50} alt="logo" />
           </Link>
         </div>
-          <ul className={'flex gap-4 h-full items-center'}>
-            {navList.map((navItem, index) => (
-              <Link key={index}
-                    href={navItem.link}
-                    className={`${poppins.className} h-full text-[rgba(70_,104_,178_,80%)] hover:font-semibold hover:text-[rgba(70_,104_,178_,100%)] tracking-wider`}>
-                <li className="relative h-full flex items-center px-4 w-max
+        <ul className={"flex gap-4 h-full items-center"}>
+          {navList.map((navItem, index) => (
+            <Link
+              key={index}
+              href={navItem.link}
+              className={`${poppins.className} h-full text-[rgba(70_,104_,178_,80%)] hover:font-semibold hover:text-[rgba(70_,104_,178_,100%)] tracking-wider`}
+            >
+              <li
+                className="relative h-full flex items-center px-4 w-max
                     before:absolute before:w-1 before:h-1 before:bg-blue-main before:bottom-0 before:left-0 before:opacity-0 before:rounded-full
-                    before:hover:w-full before:hover:opacity-100 before:transition-all before:duration-300">
-                  {navItem.name}
-                </li>
-              </Link>
-            ))}
-          </ul>
-          <Button className={'px-7'} border={true} variant={'gradient'}>Connect With Us</Button>
+                    before:hover:w-full before:hover:opacity-100 before:transition-all before:duration-300"
+              >
+                {navItem.name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+        <Button className={"px-7"} border={true} variant={"gradient"}>
+          Connect With Us
+        </Button>
       </div>
     </nav>
-  )
+  );
 }
