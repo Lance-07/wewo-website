a8dd7404352eb03c6f333bd69c36cdc1e586d52e
Lance <ballesteros.lancesamuel.bscs2021@gmail.com>
2025-02-24 17:23:26 +0800

    pls mag build ka na

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -6,9 +6,8 @@ import { Facebook, Mail } from "lucide-react";
 import Image from "next/image";
 import { poppins } from "../ui/fonts";
 import { useRouter } from "next/navigation";
-import articlesData from "./mockdata/page";
 import Footer from "../ui/components/footer";
-import { articlesDataForCards } from "./[id]/page";
+import { articlesDataForCards } from "@/lib/data";
 
 export default function ArticlesPage() {
   return (
