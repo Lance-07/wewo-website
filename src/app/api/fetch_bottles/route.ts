903bb0856de1ea3464b5f99d5f9024dbb39f7bcf
Prince Lawrence Jacinto <jacinto.princelawrence@gmail.com>
2025-03-04 00:02:39 +0800

    db sync

--- src/app/api/fetch_bottles/route.ts
+++ src/app/api/fetch_bottles/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { supabase } from "../../../../supabase";
 
 export async function POST(req: Request) {
   try {
@@ -19,17 +20,44 @@ export async function POST(req: Request) {
       body: formData
     });
 
-    console.log('Response Status:', res.status);
-    console.log('Response Headers:', res.headers);
+    // console.log('Response Status:', res.status);
+    // console.log('Response Headers:', res.headers);
 
-    console.log(typeof res)
-    console.log('Response: ', res)
+    // console.log(typeof res)
+    // console.log('Response: ', res)
 
     if (!res.ok) throw new Error("Failed to fetch data from ngrok");
 
     const data = await res.json();
-    console.log(data);
-    return NextResponse.json(data);
+  
+    
+
+    const transformedData = Object.entries(data)
+  .map(([key, value]) => ({
+    id: parseInt(key, 10), 
+    large: value.large,
+    medium: value.medium,
+    small: value.small,
+    totalLiters: value.total_liters,
+    date: value.date, 
+  }))
+  .filter(entry => entry.large !== 0 || entry.medium !== 0 || entry.small !== 0);
+
+    
+    console.log("Transformed data:", transformedData);
+    
+    // Insert into Supabase
+    const { error } = await supabase
+      .from("CollectedBottles")
+      .upsert(transformedData, { onConflict: "id" })
+    
+    if (error) {
+      console.error("Error inserting data:", error);
+    } else {
+      console.log("Data inserted successfully");
+    }
+
+    return NextResponse.json(transformedData);
   } catch (error) {
     console.error("Error fetching data:", error);
     return NextResponse.json({ error: "Failed to fetch external data" }, { status: 500 });
