9e1ec13401bc4fb3ba97e8871a53fb5e38ed1ca9
Prince Lawrence Jacinto <jacinto.princelawrence@gmail.com>
2025-03-05 01:09:42 +0800

    last ulit

--- src/app/api/fetch-bottles/route.ts
+++ src/app/api/fetch-bottles/route.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { NextResponse } from "next/server";
-=======
 import { NextRequest, NextResponse } from "next/server";
->>>>>>> 1db778b906f140a5fa2a872f1ace4b5bb720a001
 import { supabase } from "../../../../supabase";
 
 export async function GET(req: NextRequest) {
@@ -61,52 +57,11 @@ export async function POST(req: NextRequest) {
       body: formData
     });
 
-<<<<<<< HEAD
-    // console.log('Response Status:', res.status);
-    // console.log('Response Headers:', res.headers);
-
-    // console.log(typeof res)
-    // console.log('Response: ', res)
-
-    if (!res.ok) throw new Error("Failed to fetch data from ngrok");
-
-    const data = await res.json();
-  
-     
-
-    const transformedData = Object.entries(data)
-  .map(([key, value]) => ({
-    id: parseInt(key, 10), 
-    large: value.large,
-    medium: value.medium,
-    small: value.small,
-    totalLiters: value.total_liters,
-    date: value.date, 
-  }))
-  .filter(entry => entry.large !== 0 || entry.medium !== 0 || entry.small !== 0);
-
-    
-    console.log("Transformed data:", transformedData);
-    
-    // Insert into Supabase
-    const { error } = await supabase
-      .from("CollectedBottles")
-      .upsert(transformedData, { onConflict: "id" })
-    
-    if (error) {
-      console.error("Error inserting data:", error);
-    } else {
-      console.log("Data inserted successfully");
-    }
-
-    return NextResponse.json(transformedData);
-=======
     if (!rpiRes.ok) throw new Error("Failed to fetch data from ngrok");
 
     const data = await rpiRes.json();
     console.log(data);
     return NextResponse.json({ message: "Successfully fetched data." }, {status: 200});
->>>>>>> 1db778b906f140a5fa2a872f1ace4b5bb720a001
   } catch (error) {
     console.error("Error fetching data:", error);
     return NextResponse.json({ error: "Failed to fetch external data" }, { status: 500 });
