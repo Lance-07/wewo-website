955eef30c79d9eb39b0d8f08a4b150ff28584af8
Prince Lawrence Jacinto <jacinto.princelawrence@gmail.com>
2025-03-05 10:09:27 +0800

    final na talaga

--- src/app/api/fetch-turbidity/route.ts
+++ src/app/api/fetch-turbidity/route.ts
@@ -28,27 +28,4 @@ export async function POST(req: Request) {
     console.error("Error fetching data:", error);
     return NextResponse.json({ error: "Failed to fetch external data" }, { status: 500 });
   }
-
-//   const apiUrl = "https://modern-snake-evenly.ngrok-free.app/api/get_turbidity_values";
-
-//   const formData = new FormData();
-//   formData.append('date_filter', 'all');
-
-//   const res = await fetch(apiUrl, {
-//     method: "POST",
-//     headers: new Headers({
-//       'ngrok-skip-browser-warning': 'let me in',
-//     }),
-//     body: formData
-//   });
-
-//   if (!res.ok) throw new Error("Failed to fetch data from ngrok");
-
-//   const data = await res.json();
-//   console.log(data);
-//   return NextResponse.json(data);
-// } catch (error) {
-//   console.error("Error fetching data:", error);
-//   return NextResponse.json({ error: "Failed to fetch external data" }, { status: 500 });
-// }
 }
