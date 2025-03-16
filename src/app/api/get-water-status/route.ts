10281a685dbc0325d57698109b5600716787f6d5
Prince Lawrence Jacinto <jacinto.princelawrence@gmail.com>
2025-03-16 13:36:24 +0800

    added status api

--- src/app/api/get-water-status/route.ts
+++ src/app/api/get-water-status/route.ts
@@ -2,26 +2,24 @@ import { NextResponse } from "next/server";
 import { supabase } from "../../../../supabase";
 
 export async function POST(req: Request) {
-try{
-    const body = await req.json();
-    const { status } = body; 
-    console.log(status);
+    try {
+        const body = await req.json();
+        const { status } = body;
+        console.log(status);
 
         const { data, error } = await supabase
             .from('WaterLevelStatus')
-            .upsert([
-                {id: 1, status}, 
-            ])
+            .upsert([{ id: 1, status }])
             .select('status');
-        
-        if (!data){
-            return error
+
+        if (error) {
+            return NextResponse.json({ error: error.message }, { status: 500 });
         }
 
-    return NextResponse.json({"message": "received!"});
+        return NextResponse.json({ message: "Received!" }, { status: 200 });
 
-} catch (error) {
-    console.error("Error:", error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-}
+    } catch (error) {
+        console.error("Error:", error);
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
 }
